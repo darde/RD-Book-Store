5b7a960876ad8fde547001ba8cb2384c6b4bb939
darde <pablodarde@gmail.com>
2017-03-24 18:02:29 -0300

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,54 @@
-var webpack = require('webpack');
-var path = require("path"),
-  CopyWebpackPlugin = require('copy-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: {
-    babelPolyfill: 'babel-polyfill',
-    app: path.resolve(__dirname, "./src/js/App.jsx")
-  },
+  entry: './src/js/main.js',
+
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    path: './dist',
+    filename: 'bundle.js'
   },
-  devtool: 'source-map',
+
   module: {
-    preLoaders: [
-      { test: /\.jsx?$/, loader: 'eslint', exclude: /node_modules/ }
-    ],
     loaders: [
-      { test: /\.css$/, loader: "style!css" },
-      { test: /\.less$/, loader: "style!css!less" },
       {
-        test: /\.jsx?$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015'],
-          plugins: ['transform-object-rest-spread'],
+            presets: ['es2015']
         }
       },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'url?limit=10000!img?progressive=true' },
-      { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
+      {
+        test: /\.less$/,
+        loader: "style!css!less"
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        include: /img/,
+        loader: 'url'
+      },
     ]
   },
+
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: './src/index.html' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './src/vendors/phaser.min.js' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: './src/assets', to: 'assets' }
+    ])
+  ],
+
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  eslint: {
-    configFile: './.eslintrc.js',
-    failOnWarning: false,
-    failOnError: true,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'), // Tells React to build in either dev or prod modes. https://facebook.github.io/react/downloads.html (See bottom)
-      __DEV__: true
-    }),
-    new CopyWebpackPlugin([{
-      from: './src/html/index.html'
-    }]),
-    new CopyWebpackPlugin([{
-      from: './src/img', to: 'img'
-    }]),
-  ],
+
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    port: 4000,
     inline: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
+    contentBase: './dist',
+    port: 5000
   }
 };
